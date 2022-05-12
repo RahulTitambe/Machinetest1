72b5ec5561d798a334447c778ca730ad61408cdc
RahulTitambe <rt.rahultitambe@gmail.com>
2022-05-12 11:13:35 +0530

    Machine Test 1

--- app/src/main/java/com/admedia/machinetest1/adapters/EmployAdapter.kt
+++ app/src/main/java/com/admedia/machinetest1/adapters/EmployAdapter.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.admedia.machinetest1.R
 import com.admedia.machinetest1.data.Employee
+import com.admedia.machinetest1.databinding.ImageViewBinding
 import com.admedia.machinetest1.databinding.RecyclerMainContainerBinding
 
 class EmployAdapter(var EmployeeList : ArrayList<Employee>) : RecyclerView.Adapter<EmployAdapter.EmployeeViewHolder>() {
@@ -22,12 +24,20 @@ class EmployAdapter(var EmployeeList : ArrayList<Employee>) : RecyclerView.Adapt
         fun OnImageClick(imageId : Int, position: Int)
     }
 
+    var onDetailClickListener : OnDetailClickListener? = null
+    var onImageClickListener : OnImageClickListener? = null
+
+    var img : ImageView? = null
+
     inner class EmployeeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
         var imgEmpImage = itemView.findViewById<ImageView>(R.id.imgImage)
         var txtEmpName = itemView.findViewById<TextView>(R.id.txtName)
         var txtEmpId = itemView.findViewById<TextView>(R.id.txtID)
         var txtEmpEmail = itemView.findViewById<TextView>(R.id.txtEmail)
+        var mainContainer = itemView.findViewById<ConstraintLayout>(R.id.mainContainer)
+        var imgImageViewImage = itemView.findViewById<ImageView>(R.id.imgImageViewImage)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
@@ -40,8 +50,17 @@ class EmployAdapter(var EmployeeList : ArrayList<Employee>) : RecyclerView.Adapt
         holder.txtEmpName.text = "${EmployeeList[position].Name}"
         holder.txtEmpId.text = "${EmployeeList[position].Id}"
         holder.txtEmpEmail.text = "${EmployeeList[position].Email}"
+
+        holder.mainContainer.setOnClickListener {
+            onDetailClickListener?.OnDetailClick(EmployeeList.get(position), position)
+        }
+
+        holder.imgImageViewImage.setOnClickListener {
+            onImageClickListener?.OnImageClick(EmployeeList.get(position).Image, position)
+        }
     }
 
     override fun getItemCount(): Int = EmployeeList.size
 
 }
+
